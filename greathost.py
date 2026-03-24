1377e99206fd3d7498dc9d310dea9a8c78a7f3de
heyzone <harrywujy520@gmail.com>
2026-03-24 14:21:44 +0800

    Update greathost.py

--- greathost.py
+++ greathost.py
@@ -14,7 +14,7 @@ PASSWORD = os.getenv("GREATHOST_PASSWORD", "")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
 PROXY_URL = os.getenv("PROXY_URL", "")
-TARGET_NAME = os.getenv("TARGET_NAME", "loveMC")
+TARGET_NAME = os.getenv("TARGET_NAME", "kv1")
 
 STATUS_MAP = {
     "running": ["🟢", "Running"],
